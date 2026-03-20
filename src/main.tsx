d1c38413f4997c34a7bc2fc3c1713539b29bc80d
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2026-03-20 13:55:05 +0200

    Provider is used to fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./redux/store";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 );
